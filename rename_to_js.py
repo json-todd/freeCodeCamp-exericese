53e9c0b5e2e9af3718795ab5ea43ce1cd2daab60
Tri Anh Phung <47502387+json-todd@users.noreply.github.com>
2021-04-05 20:04:08 +0300

    Update rename_to_js.py

--- rename_to_js.py
+++ rename_to_js.py
@@ -5,8 +5,7 @@ Re-define the extension of all files in the folder as .js
 
 import os 
 
-path = str(input('The path of your folder: '))
-path = path.replace('\','/')
+path = input('The path of your folder: ')).replace('\\','/')
 
 os.chdir(path)
 
