4cb39ff9a02af25bf9e45e6cf66afa1e88979a9f
Tri Anh Phung <47502387+json-todd@users.noreply.github.com>
2021-09-12 17:51:02 +0300

    Create time_calc_1.py

---
+++ python/time_calc/time_calc_1.py
@@ -0,0 +1,24 @@
+def add_time(start_time, duration, start_day = False):
+    
+    # data structures
+    new_time = []
+    
+    start_time_format = start_time.split(" ") # [time, AM/PM]
+    
+    def getTimeAndHour(time):
+        """ Receive input as time format HH:MM
+        return the hour and minute
+        """
+        time_arr = time.split(":")
+        return time_arr[0], time_arr[1]
+ 
+    start_hour_min = [int(t) for t in getTimeAndHour(start_time_format[0])]
+    duration_hour_min = [int(t) for t in getTimeAndHour(duration)]
+
+    for i in range(2):
+        new_time.append(start_hour_min[i] + duration_hour_min[i])
+
+    # Logic for conversion of minute to hour
+    if new_time[1] >= 60:
+        new_time[0] += 1
+        new_time[1] -= 60
