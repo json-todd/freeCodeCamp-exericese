743c57b71c56258cd3de18fde3a2484eb656085a
Tri Anh Phung <47502387+json-todd@users.noreply.github.com>
2021-09-21 21:44:59 +0300

    Update budget_app_1.py

--- budget_app/budget_app_1.py
+++ budget_app/budget_app_1.py
@@ -4,6 +4,8 @@ class Category:
         self.ledger = []
         
     def get_balance(self):
+        """Returns current balance of budget category
+        """
         self.balance = 0
         for transact in self.ledger:
             self.balance += transact.get('amount',0)
@@ -11,11 +13,14 @@ class Category:
         return float(self.balance)
 
     def deposit(self, amount,description = ""):
-        """
+        """ Accepts amount and description
+        Description is empty string by default
+        Append an object to the ledget list in form of:
+        {"amount": amount, "description": description}
         """
         self.ledger.append(
             {
-                'amount': amount,
+                'amount': float(amount),
                 'description': description
             }
         )
@@ -30,7 +35,10 @@ class Category:
         else: return True
 
     def withdraw(self, amount, desciption = ""):
-        """
+        """Accepts amount and description
+        Description is empty string by default
+        Append an object to the ledget list in negative value of amount:
+        {"amount": -amount, "description": description}
         """
         if self.check_funds(amount):
         
@@ -54,3 +62,34 @@ class Category:
 
         else:
             return False
+
+    def __str__(self):
+        """ Object should be printed with style
+        * A title line of 30 characters
+        where the name of the category is centered in a line of * characters.
+
+        * A list of the items in the ledger.
+        Each line should show the description and amount.
+        
+        * A line displaying the category total.
+        """
+        def makeTitle(name):
+            title = ''
+            place_holder = '*' * round((30-len(name))/2)
+            title = place_holder + name + place_holder
+            return title + '\n'
+        title = makeTitle(self.name)
+        
+        transactions = ''
+        for transact in self.ledger:
+            desc = transact.get('description')[0:23]
+            amt = format(transact.get('amount'),'.2f') # float function returns a string
+            if len(amt) > 7:
+                amt = amt[0:7]
+
+            place_holder = ' ' * round(30-len(desc)-len(amt))
+            transactions +=  desc + place_holder + amt + '\n'
+        
+        total = f'Total: {round(self.get_balance(),2)}'
+
+        return title + transactions + total
